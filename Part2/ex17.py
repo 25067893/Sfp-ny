59def94c1e39be46912acc3c113c788638855b9f
25067893 <yning0541@gmail.com>
2025-07-22 03:19:44 +0000

    work done for day2

---
+++ Part2/ex17.py
@@ -0,0 +1,10 @@
+# TODO: Refer to the objective and sample output and figure out your own code!
+# Time to graduate :p
+
+# Let's ask the user their name and graduation year
+name = input("Enter your name: ")
+year = input("Enter your graduation year: ")
+
+# Print a celebratory message
+print(f"Congratulations, {name}! You are graduating in {year}! 🎓🎉")
+print("Wishing you all the best in your future endeavors!")
